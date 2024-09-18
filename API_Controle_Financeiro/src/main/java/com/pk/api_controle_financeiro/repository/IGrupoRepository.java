18dc2ed8fe8352405bedb32855f0d2b1f7c42f8d
Pedro Henrique de Jesus Sousa <pedrohjs11@gmail.com>
2024-09-17 22:01:08 -0300

    Repositórios criados
    Repositórios das classes criados

--- API_Controle_Financeiro/src/main/java/com/pk/api_controle_financeiro/repository/IGrupoRepository.java
+++ API_Controle_Financeiro/src/main/java/com/pk/api_controle_financeiro/repository/IGrupoRepository.java
@@ -1,9 +1,9 @@
 package com.pk.api_controle_financeiro.repository;
 
-import com.pk.api_controle_financeiro.model.Pessoa;
+import com.pk.api_controle_financeiro.model.Grupo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface IGrupoRepository extends JpaRepository<Grupo, Long> {
 }
--- API_Controle_Financeiro/src/main/java/com/pk/api_controle_financeiro/repository/ILancamentoRepository.java
+++ API_Controle_Financeiro/src/main/java/com/pk/api_controle_financeiro/repository/ILancamentoRepository.java
@@ -1,9 +1,9 @@
 package com.pk.api_controle_financeiro.repository;
 
-import com.pk.api_controle_financeiro.model.Pessoa;
+import com.pk.api_controle_financeiro.model.Lancamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface IPessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface ILancamentoRepository extends JpaRepository<Lancamento, Long> {
 }
