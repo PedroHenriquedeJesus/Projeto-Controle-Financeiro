094e6eb76ce3533d727146aee38d9d2b08906eff
Pedro Henrique de Jesus Sousa <pedrohjs11@gmail.com>
2024-09-18 21:35:47 -0300

    Criados os componentes pessoa, grupo, meta e lançamento

---
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/lancamento-component/lancamento-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lancamento-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './lancamento-component.component.html',
+  styleUrl: './lancamento-component.component.css'
+})
+export class LancamentoComponentComponent {
+
+}
---
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/pessoa-component/pessoa-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pessoa-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './pessoa-component.component.html',
+  styleUrl: './pessoa-component.component.css'
+})
+export class PessoaComponentComponent {
+
+}
