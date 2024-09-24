d7395643764679e3aa87064eaa6673be63f06f0a
KaykySG <kaykyfsoares@hotmail.com>
2024-09-24 20:59:08 -0300

    Organizando rotas iniciais do sistema

--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/app.routes.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { Routes, RouterModule} from '@angular/router';
+import { NgModule } from '@angular/core';
+import {PessoaComponentComponent} from "./pessoa-component/pessoa-component.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path:'home', component:PessoaComponentComponent}
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
