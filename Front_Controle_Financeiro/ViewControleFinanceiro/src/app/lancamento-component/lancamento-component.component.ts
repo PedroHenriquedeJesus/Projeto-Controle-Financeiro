54eb54df3d66d31874fbcab77de45410d68531d5
KaykySG <kaykyfsoares@hotmail.com>
2024-09-25 10:53:37 -0300

    Iniciando Card para Cadastro de Lancamento

--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/lancamento-component/lancamento-component.component.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/lancamento-component/lancamento-component.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {FloatLabelModule} from 'primeng/floatlabel'
 
 @Component({
   selector: 'app-lancamento-component',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule],
   templateUrl: './lancamento-component.component.html',
   styleUrl: './lancamento-component.component.css'
 })
 export class LancamentoComponentComponent {
+
 
 }
