acb9e80d0a39ac6673ceee8df022a2cd6272b38d
Pedro Henrique de Jesus Sousa <pedrohjs11@gmail.com>
2024-10-01 23:18:17 -0300

    Adicionados os componentes das telas de: Grupo, Meta e Pessoa

--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/meta-component/meta-component.component.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/meta-component/meta-component.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {FloatLabelModule} from 'primeng/floatlabel'
 
 @Component({
   selector: 'app-meta-component',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule],
   templateUrl: './meta-component.component.html',
   styleUrl: './meta-component.component.css'
 })
--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/pessoa-component/pessoa-component.component.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/pessoa-component/pessoa-component.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {CardModule} from 'primeng/card';
+import {ButtonModule} from 'primeng/button';
+import {InputTextModule} from 'primeng/inputtext';
+import {FloatLabelModule} from 'primeng/floatlabel'
+import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-pessoa-component',
   standalone: true,
-  imports: [],
+  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule, InputMaskModule],
   templateUrl: './pessoa-component.component.html',
   styleUrl: './pessoa-component.component.css'
 })
