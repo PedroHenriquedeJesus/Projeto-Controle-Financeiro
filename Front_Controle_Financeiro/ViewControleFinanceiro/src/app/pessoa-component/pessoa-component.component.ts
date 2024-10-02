5bc2cff8b964cfffe5845a02829af337a2889314
KaykySG <kaykyfsoares@hotmail.com>
2024-10-02 20:02:39 -0300

    Implementando tratativas de erro para a tela pessoa

--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/pessoa-component/pessoa-component.component.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/pessoa-component/pessoa-component.component.ts
@@ -1,17 +1,54 @@
 import { Component } from '@angular/core';
-import {CardModule} from 'primeng/card';
-import {ButtonModule} from 'primeng/button';
-import {InputTextModule} from 'primeng/inputtext';
-import {FloatLabelModule} from 'primeng/floatlabel'
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Importar CommonModule
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { FloatLabelModule } from 'primeng/floatlabel';
 import { InputMaskModule } from 'primeng/inputmask';
 
 @Component({
   selector: 'app-pessoa-component',
   standalone: true,
-  imports: [CardModule, ButtonModule, InputTextModule, FloatLabelModule, InputMaskModule],
+    imports: [
+      FormsModule,
+      CommonModule, // Adicionar CommonModule
+      CardModule,
+      InputTextModule,
+      InputTextareaModule,
+      ButtonModule,
+      FloatLabelModule,
+      InputMaskModule,
+    ],
   templateUrl: './pessoa-component.component.html',
   styleUrl: './pessoa-component.component.css'
 })
 export class PessoaComponentComponent {
 
+  nome: string = '';
+  CPF: string = '';
+  email: string = '';
+  telefone: string = '';
+
+
+  onSubmit(form: NgForm) {
+    if (form.valid) {
+      console.log('Formulário enviado', {
+        nome: this.nome,
+        CPF: this.CPF,
+        email: this.email,
+        telefone: this.telefone
+      });
+
+      // Limpar os campos do formulário
+      this.nome = '';
+      this.CPF = '';
+      this.email = '';
+      this.telefone = '';
+      form.resetForm();
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
 }
