7dbd72e972cf0e48c28e6f39c3528e61d21307d4
Vinicius Fernandes da Silva <107755602+Anvicii@users.noreply.github.com>
2024-10-03 11:19:40 -0300

    Tela de Home funcional
    tela home funcional com o TabMenu já redirecionando para as outras paginas.

--- Front_Controle_Financeiro/ViewControleFinanceiro/src/app/home-component/home-component.component.ts
+++ Front_Controle_Financeiro/ViewControleFinanceiro/src/app/home-component/home-component.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Router } from '@angular/router';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [ButtonModule, CardModule, TabMenuModule],
   templateUrl: './home-component.component.html',
-  styleUrl: './home-component.component.css'
+  styleUrls: ['./home-component.component.css']
 })
 export class HomeComponentComponent {
+  items: MenuItem[];
 
+  constructor(private router: Router) {
+    this.items = [
+      { label: 'Pessoa', icon: 'pi pi-users', command: () => this.navigateTo('pessoa') },
+      { label: 'Meta', icon: 'pi pi-chart-line', command: () => this.navigateTo('meta') },
+      { label: 'Lançamento', icon: 'pi pi-calendar', command: () => this.navigateTo('lancamento') },
+      { label: 'Grupo', icon: 'pi pi-users', command: () => this.navigateTo('grupo') }
+    ];
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
 }
