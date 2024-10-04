b7f28e9b3e81524d90cc6ce7e441d13ecc5458bd
Pedro Henrique de Jesus Sousa <pedrohjs11@gmail.com>
2024-10-03 21:35:18 -0300

    Criado Lancamento

---
+++ API_Controle_Financeiro/src/main/java/com/pk/api_controle_financeiro/model/Lancamento.java
@@ -0,0 +1,117 @@
+package com.pk.api_controle_financeiro.model;
+import com.pk.api_controle_financeiro.enums.Categoria;
+import com.pk.api_controle_financeiro.enums.TipoLancamento;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "lancamento")
+
+public class Lancamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 255)
+    private String descricao;
+
+    @Column(nullable = false)
+    private double valor;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Enumerated(EnumType.STRING)
+    private TipoLancamento tipo;
+
+    @Enumerated(EnumType.STRING)
+    private Categoria categoria;
+
+    public Lancamento(Long id, String nome, String descricao, double valor, LocalDate data, TipoLancamento tipo, Categoria categoria) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.data = data;
+        this.tipo = tipo;
+        this.categoria = categoria;
+    }
+
+    public Lancamento() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public TipoLancamento getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoLancamento tipo) {
+        this.tipo = tipo;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    @Override
+    public String toString() {
+        return "Lancamento{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", valor=" + valor +
+                ", data=" + data +
+                ", tipo=" + tipo +
+                ", categoria=" + categoria +
+                '}';
+
+    }
+}
